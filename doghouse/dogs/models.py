55e8824958e0d7b29097eac483de3733b81861e1
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-03 14:50:21 +0300

    add choices for gender

--- doghouse/dogs/models.py
+++ doghouse/dogs/models.py
@@ -39,10 +39,12 @@ class Breed(models.Model):
 
 class Dog(models.Model):
 
+    GENDERS = {"M": "Male", "F": "Female"}
+
     name = models.CharField(max_length=70)
     age = models.PositiveIntegerField()
     breed = models.ForeignKey("Breed", on_delete=models.SET_NULL, null=True)
-    gender = models.CharField(max_length=20)
+    gender = models.CharField(max_length=8, choices=GENDERS, blank=False, default="M")
     color = models.CharField(max_length=30)
     favorite_food = models.CharField(max_length=70)
     favorite_toy = models.CharField(max_length=70)
