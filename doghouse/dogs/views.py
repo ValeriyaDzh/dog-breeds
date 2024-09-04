e127520e4fbb99323ec558cf44ceda87b3478fcb
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-04 15:08:53 +0300

    add DogList

--- doghouse/dogs/views.py
+++ doghouse/dogs/views.py
@@ -5,21 +5,21 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
 
-from .models import Breed, Dog
-from .serializers import BreedSerializer, DogSerializer
+from .models import Dog, Breed
+from .serializers import DogSerializer, BreedSerializer
 
 
-class BreedList(APIView):
+class DogList(APIView):
 
     def get(self, request: Request, format=None) -> Response:
 
-        breeds = Breed.objects.all()
-        serialiser = BreedSerializer(breeds, many=True)
+        Dogs = Dog.objects.all()
+        serialiser = DogSerializer(Dogs, many=True)
         return Response(serialiser.data, status=status.HTTP_200_OK)
 
     def put(self, request: Request, format=None) -> Response:
 
-        serializer = BreedSerializer(data=request.data)
+        serializer = DogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
