2c50739574bfd1b31f59e4882bfb5e816f9345d9
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-04 15:24:56 +0300

    add DogDetail

--- doghouse/dogs/views.py
+++ doghouse/dogs/views.py
@@ -12,13 +12,11 @@ from .serializers import DogSerializer, BreedSerializer
 class DogList(APIView):
 
     def get(self, request: Request, format=None) -> Response:
-
         Dogs = Dog.objects.all()
         serialiser = DogSerializer(Dogs, many=True)
         return Response(serialiser.data, status=status.HTTP_200_OK)
 
     def put(self, request: Request, format=None) -> Response:
-
         serializer = DogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -26,3 +24,32 @@ class DogList(APIView):
 
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DogDetail(APIView):
+
+    def _get_dog(self, pk: int) -> Dog:
+        try:
+            return Dog.objects.get(pk=pk)
+
+        except Dog.DoesNotExist:
+            raise Http404
+
+    def get(self, request: Request, pk: int, format=None) -> Response:
+        dog = self._get_dog(pk)
+        serializer = DogSerializer(dog)
+        return Response(serializer, status=status.HTTP_200_OK)
+
+    def put(self, request: Request, pk: int, format=None) -> Response:
+        dog = self._get_dog(pk)
+        serializer = DogSerializer(dog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, pk: int, format=None) -> Response:
+        dog = self._get_dog(pk)
+        dog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
