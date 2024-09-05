af763222a82de60c9012939aa9af36d7d558ef42
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-05 13:59:18 +0300

    add BreedList url

--- doghouse/dogs/urls.py
+++ doghouse/dogs/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import DogList, DogDetail
+# from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from .views import DogList, DogDetail, BreedList
+
+router = DefaultRouter()
+router.register("breeds", BreedList, "breeds")
 
 urlpatterns = [
     path("dogs/", DogList.as_view()),
     path("dogs/<int:pk>", DogDetail.as_view()),
+    path("", include(router.urls)),
 ]
